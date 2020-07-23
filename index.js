296cee419e463bd5245cfac9905477ad4a93feaf
polina.voronova <polonium.pv@gmail.com>
2020-07-23 17:09:17 +0300

    feat: implement validateExpression function

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const expression = '2 * ( 3 + 6 / 2 ) / 4';
 
 export function convertToRPN(expression) {
+    validateExpression(expression);
 
+    let symbols = expression.split(' ');
+
+    for (let value of symbols) {
+        if (skobka) {
+            continue
+        }
+
+        if (drugaya skobka) {
+            continue
+        }
+
+        if (chislo) {
+            continue
+        }
+
+        //ddeystviye dlya operatorov
+        
+     }
 
 }
 
@@ -10,39 +29,31 @@ function validateExpression(expression) {
         throw new TypeError("A value is not of the expected type: " + expression);
     }
 
-    let arrayOfExpressionSymbol = expression.split(' ');
-
-    for (let value of arrayOfExpressionSymbol) {
-        if (isNaN(+value)) { //Проверка на операнды (при попытке приведения к числу - не NaN) --- можно складывать 
-            if (value === '+' || value === '-' || value === '*' || value === '/') { //Проверка на "+" "-" "/" "*"
-                //console.log('true');
-            }
-        } else {
-            if (+value > 0) { //Проверка на положительные числа
-
-            }
+    let symbols = expression.split(' ');
+    for (let value of symbols) {
+        if (!isNaN(+value) && +value > 0) { //Проверка на числа, положительные числа
+            continue;
         }
+
+        const allowedSymbols = {
+            '+' : true,
+            '-' : true,
+            '*' : true,
+            '/' : true,
+            '(' : true,
+            ')' : true
+        };
+
+        if (allowedSymbols.hasOwnProperty(value)) { //Проверка на "+" "-" "/" "*" "(" ")" на наличие ключа в объекте
+            continue;
+        }
+
+        //функция для проверки валидности порядка скобок (пробегаю, закидываю в стек и при встрече следующей проверяю на парность)
+
+        throw new Error('Invalid expression: invalid parentheses or double operators or restricted symbols');
     }
 }
 
 export function calculateRPNExpression(expression) {
 
-}
-
-
-let s = '2 + 3 * ( 5 - 2 )';
-let n = s.split(' ');
-console.log(n);
-for (let i of n) {
-    if (isNaN(+i)) { //Проверка на операнды (при попытке приведения к числу - не NaN)
-        if (i === '+' || i === '-' || i === '*' || i === '/') { //Проверка на "+" "-" "/" "*"
-            console.log('true');
-        }
-        //continue;
-    } else {
-        if (+i > 0) { //Проверка на положительные числа
-
-        }
-        //console.log(+i)
-    }
 }
