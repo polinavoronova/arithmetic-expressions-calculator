2784def6c5f18087acdfa46a96b296f6a3233651
polina.voronova <polonium.pv@gmail.com>
2020-08-02 17:40:09 +0300

    fix convertToRPN

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 const expression = '2 * ( 3 + 6 / 2 ) / 4';
 
 export function convertToRPN(expression) {
-    validateExpression(expression);
-
     let symbols = expression.split(' ');
+    let RPNExpression = [];
+    let stack = [];
+    const operationPriority = {
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2,
+    }
 
     for (let value of symbols) {
-        if (skobka) {
+        if (value === '(') {
+            stack.push(value);
             continue
         }
 
-        if (drugaya skobka) {
+        if (value === ')') {
+            for (let i = stack.length - 1; i >= 0; i--) {
+                if (stack[i] === '(') {
+                    stack = stack.splice(0, i);
+                    break
+                }
+
+                RPNExpression.push(stack[i]);
+            }
             continue
         }
 
-        if (chislo) {
+        if (!isNaN(+value)) {  //number
+            RPNExpression.push(value);
             continue
         }
 
-        //ddeystviye dlya operatorov
-        
-     }
 
+        let lastStackSymbol = stack[stack.length - 1];
+        if (operationPriority[lastStackSymbol] > operationPriority[value] || //operators 
+            operationPriority[lastStackSymbol] === operationPriority[value]) {
+            RPNExpression.push(lastStackSymbol);
+            stack.pop();
+            stack.push(value);
+            continue
+        }
+
+        stack.push(value);
+    }
+
+    for (let i = stack.length - 1; i >= 0; i--) {
+        RPNExpression.push(stack[i]);
+    }
+
+    return RPNExpression;
 }
 
 function validateExpression(expression) {
@@ -36,12 +66,12 @@ function validateExpression(expression) {
         }
 
         const allowedSymbols = {
-            '+' : true,
-            '-' : true,
-            '*' : true,
-            '/' : true,
-            '(' : true,
-            ')' : true
+            '+': true,
+            '-': true,
+            '*': true,
+            '/': true,
+            '(': true,
+            ')': true
         };
 
         if (allowedSymbols.hasOwnProperty(value)) { //Проверка на "+" "-" "/" "*" "(" ")" на наличие ключа в объекте
